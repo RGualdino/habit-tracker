15bf0712d55b9e94ab717f93cd997171275851ec
Rúben Gualdino <ruben.gualdino@ctw.bmwgroup.com>
2026-09-10 11:37:48 +0100

    feature: add JWT authentication

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,17 @@
 import { ConflictException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { RegisterDto } from './dto/register.dto';
 import { Prisma } from '../generated/prisma/client';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async register(registerDto: RegisterDto) {
     const hashedPassword = await bcrypt.hash(registerDto.password, 10);
@@ -33,5 +38,34 @@ export class AuthService {
 
       throw error;
     }
+  }
+
+  async login(logindto: LoginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: logindto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ConflictException('Invalid email');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      logindto.password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new ConflictException('Invalid password');
+    }
+
+    const payload = {
+      sub: user.id,
+      email: user.email,
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
